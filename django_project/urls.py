8d2231a6d780da397754983fadee2752998adf91
Tyler Lewis <tylerlewis182@gmail.com>
2019-10-16 20:03:02 -0700

    finished lecture 2 - application and routes

--- django_project/urls.py
+++ django_project/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')), # when we go to: localhost:8000/, sends us to: blog/urls.py views.
 ]
