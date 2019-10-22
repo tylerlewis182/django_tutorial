a13bc11041779dd29512d0570c25bf4e4d1dd649
Tyler Lewis <tylerlewis182@gmail.com>
2019-10-22 14:29:41 -0700

    finished section 6

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,13 @@
 from django.shortcuts import render
-
-posts = [
-  {
-    'author': 'CoreyMS',
-    'title': 'Blog Post 1',
-    'content': 'First Post Content',
-    'date_posted': 'August 27, 2018',
-  },
-  {
-    'author': 'Jane Doe',
-    'title': 'Blog Post 2',
-    'content': 'Second Post Content',
-    'date_posted': 'August 28, 2018',
-  }
-]
+from .models import Post
 
 
 def home(request):
   context = {
-    'posts': posts,
+    'posts': Post.objects.all(),
   }
   return render(request, 'blog/home.html', context)
 
 def about(request):
   return render(request, 'blog/about.html', {'title': 'About'})
+
